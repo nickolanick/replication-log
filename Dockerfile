bb17a2172d1ef0ef4bec27d82bf1cbb187c9d5e4
iyuroch <iyuroch@gmail.com>
2021-10-24 23:25:50 +0300

    fix follower list parsing

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3
 
-COPY requirements.txt requirements.txt 
 WORKDIR /python
+COPY requirements.txt requirements.txt 
 
 RUN pip3 install -r requirements.txt
 
--- test.sh
+++ test.sh
@@ -4,8 +4,10 @@ curl -X POST http://127.0.0.1:8080/ \
    -H 'Content-Type: application/json' \
    -d '{"message":"my_great_message"}'
 
-#curl -X POST http://127.0.0.1:8080/ \
-#   -H 'Content-Type: application/json' \
-#   -d '{"message":"another_great_message"}'
+curl -X POST http://127.0.0.1:8080/ \
+   -H 'Content-Type: application/json' \
+   -d '{"message":"another_great_message"}'
 
-#curl -X GET http://127.0.0.1:8080/
+curl -X GET http://127.0.0.1:8080/
+
+# ./vegeta attack -targets=vegeta -duration=10s | tee results.bin | vegeta plot
