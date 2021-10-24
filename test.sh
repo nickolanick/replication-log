31940128c5505dde8c9da2dfa0baa37e6921ade2
iyuroch <iyuroch@gmail.com>
2021-10-24 19:27:07 +0300

    fix test for docker-compose

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl -X POST http://127.0.0.1:5000/ \
+curl -X POST http://127.0.0.1:8080/ \
    -H 'Content-Type: application/json' \
    -d '{"message":"my_great_message"}'
 
-curl -X POST http://127.0.0.1:5000/ \
+curl -X POST http://127.0.0.1:8080/ \
    -H 'Content-Type: application/json' \
    -d '{"message":"another_great_message"}'
 
-curl -X GET http://127.0.0.1:5000/
+curl -X GET http://127.0.0.1:8080/
